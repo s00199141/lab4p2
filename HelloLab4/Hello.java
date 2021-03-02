c3df7acd8695e4af948a7c9c4089f8bb82389916
S00199141@mail.itsligo.ie <Ali Sakkar>
2021-03-02 12:21:26 +0000

    Initial version

---
+++ HelloLab4/Hello.java
@@ -0,0 +1,16 @@
+
+/**
+ * This is a git file test.
+ *
+ * @author (Ali Sakkar)
+ * @version 1.0
+ */
+public class Hello
+{
+    public static void main(String args[])
+    {
+        String name = "Hi There";
+        
+        System.out.println(name);
+    }
+}   
